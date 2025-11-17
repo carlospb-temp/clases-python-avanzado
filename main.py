cf037a073a2e487a43b7549060ace821785b2cfb
Carlos Pabe <pabescaladedomayor@gmail.com>
2025-11-17 19:51:18 +0100

    feat: primer ejercicio

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-# Aqui esta el codigo principal
+from dataclasses import dataclass, field
+from collections import deque
 
+@dataclass
+class Cola:
+    tareas: deque = field(default_factory=deque)
+    
+    def añadir_tarea(self, tarea):
+        self.tareas.append(tarea)
+        
+    def siguiente_tarea(self):
+        return self.tareas.popleft()
+    
+    def tareas_pendientes(self):
+        pass
+    
+
+if __name__ == "__main__":
+    cola = Cola()
+
+    cola.añadir_tarea("procesar_imagen")
+    cola.añadir_tarea("backup")
+    cola.añadir_tarea("exportar_pdf")
+
+    print(cola)  # Cola(pendientes=3)
+
+    print(cola.siguiente_tarea())  # procesar_imagen
+    print(cola.siguiente_tarea())  # backup
+
+    print(cola)  # Cola(pendientes=1)
+ 
